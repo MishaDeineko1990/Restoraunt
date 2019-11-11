c0d63609bc2b8333b56144c71367d4fc5f3d13b5
MishaDeineko1990 <deinekomisha1990@gmail.com>
2019-11-11 20:44:49 +0000

    first commit

---
+++ src/script/script.js
@@ -0,0 +1,113 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fix demo gallery
+
+// const gallery_boxes = $(".list-unstyled");
+
+// const gallery_boxes = document.querySelecctorAll('img');
+
+
+// gallery_boxes.forEach(function(item, i, gallery_boxes) {
+//   console.log( i + ": " + item + " (массив:" + arr + ")" );
+// });
+
+
+// const options =
+//  {
+// 	root: null,
+// 	rootMargin: '0px',
+// 	threshold: 0.1
+// }
+
+// function handle_galery(g_box, observer){
+// 	g_box.forEach(my_g_box =>{
+// 		console.log(my_g_box.intersectionRatio);
+// 		if (my_g_box.intersectionRatio > 0) {
+// 			plus_id(my_g_box.target);
+// 		}
+// 		else{
+// 			minus_id(my_g_box.target);
+// 		}
+// 	})
+// }
+
+// function plus_id(){
+
+// }
+
+
+// function minus_id(){
+
+// }
+
+// const observer = new IntersectionObserver(handle_galery, options);
+
+// gallery_boxes.forEach(boxx =>{
+// 	observer.observe(boxx);
+// })
+
